a8eff4eac827689fa4ebc250f216ce720445cc1e
Cara Cheng <cara.yuhua@gmail.com>
2018-08-20 17:44:22 -0700

    Preliminary layout designs

--- app/src/main/java/com/example/android/succulentgarden/MainActivity.java
+++ app/src/main/java/com/example/android/succulentgarden/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.succulentgarden;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button generalCare = (Button) findViewById(R.id.generalcare);
+        generalCare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent genCareIntent = new Intent(MainActivity.this, generalCareGuide.class);
+                startActivity(genCareIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/android/succulentgarden/generalCareGuide.java
@@ -0,0 +1,13 @@
+package com.example.android.succulentgarden;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class generalCareGuide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_care_guide);
+    }
+}
