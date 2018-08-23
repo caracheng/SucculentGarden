02d236e303c3c3c4671f0bdff396654048786fb6
Cara Cheng <cara.yuhua@gmail.com>
2018-08-23 12:04:41 -0700

    Added careGuide class for future use when database is incorporated and activities for indoorCare, outdoorCare and warningSigns.

--- app/src/main/java/com/example/android/succulentgarden/generalCareGuide.java
+++ app/src/main/java/com/example/android/succulentgarden/generalCareGuide.java
@@ -1,7 +1,10 @@
 package com.example.android.succulentgarden;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class generalCareGuide extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class generalCareGuide extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_care_guide);
+
+        Button indoorCare = (Button) findViewById(R.id.indoorcare);
+        indoorCare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent indoorCareIntent = new Intent(generalCareGuide.this,indoorCare.class);
+                startActivity(indoorCareIntent);
+            }
+        });
+
+        Button outdoorCare = (Button) findViewById(R.id.outdoorcare);
+        outdoorCare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent outdoorCareIntent = new Intent(generalCareGuide.this,outdoorCare.class);
+                startActivity(outdoorCareIntent);
+            }
+        });
+
+        Button warningSigns = (Button) findViewById(R.id.warningsigns);
+        warningSigns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent warningSignsIntent = new Intent(generalCareGuide.this,warningSigns.class);
+                startActivity(warningSignsIntent);
+            }
+        });
     }
 }
